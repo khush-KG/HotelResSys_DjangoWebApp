180b366e40aa9f289a27d3c78bc7efc2ccac8887
Kushagra Garg <kgtaraori@gmail.com>
2023-11-06 18:09:17 +0530

    Database migrated to MySQL ;)

--- Lotus_Delights_Hotels_and_Dinings/settings.py
+++ Lotus_Delights_Hotels_and_Dinings/settings.py
@@ -75,13 +75,23 @@ WSGI_APPLICATION = 'Lotus_Delights_Hotels_and_Dinings.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'django_hrs',
+        'USER': 'root',
+        'PASSWORD': 'Solantaraori@1',
+        'HOST': 'localhost',  # or the hostname where your MySQL server is running
+        'PORT': '3306',      # or the port on which your MySQL server is listening
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/4.1/ref/settings/#auth-password-validators
